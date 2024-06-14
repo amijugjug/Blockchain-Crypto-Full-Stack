40da1f9bfeb2c726d1bd5f2db09c51fea783b938
Prakhar Srivastava <sriprakhar2@gmail.com>
2024-06-15 00:36:10 +0530

    feat: block-chaing backend started

---
+++ BE-blockchain/main.js
@@ -0,0 +1,72 @@
+import SHA256 from "crypto-js";
+
+class Block {
+  constructor(index, timestamp, data, previousHash) {
+    this.index = index;
+    this.timestamp = timestamp;
+    this.data = data;
+    this.previousHash = previousHash;
+    this.hash = this.calculateHash();
+  }
+
+  calculateHash() {
+    return SHA256.SHA256(
+      this.index +
+        this.previousHash +
+        this.timestamp +
+        JSON.stringify(this.data)
+    ).toString();
+  }
+}
+
+class BlockChain {
+  constructor() {
+    this.chain = [this.createGenesisBlock()];
+  }
+
+  createGenesisBlock() {
+    return new Block(0, new Date().getTime(), "Genesis Block", "0");
+  }
+
+  getLatestBlock() {
+    return this.chain[this.chain.length - 1];
+  }
+
+  addBlock(newBlock) {
+    newBlock.previousHash = this.getLatestBlock().hash;
+    newBlock.hash = newBlock.calculateHash();
+    this.chain.push(newBlock);
+  }
+
+  isChainValid() {
+    for (let i = 1; i < this.chain.length; i++) {
+      const currentBlock = this.chain[i];
+      const previousBlock = this.chain[i - 1];
+
+      if (currentBlock.hash !== currentBlock.calculateHash()) {
+        return false;
+      }
+
+      if (currentBlock.previousHash !== previousBlock.hash) {
+        return false;
+      }
+    }
+    return true;
+  }
+}
+
+const newBlockChain = new BlockChain();
+newBlockChain.addBlock(
+  new Block(1, new Date().getTime(), {
+    amount: 1,
+    owner: "ABC",
+  })
+);
+newBlockChain.addBlock(
+  new Block(2, new Date().getTime(), {
+    amount: 5,
+    owner: "DEF",
+  })
+);
+
+console.log(JSON.stringify(newBlockChain, null, 2));
